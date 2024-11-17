58d727a11e70af59c145b77ba651a31e59524e69
Jonathan Alonzo García González <L20290843@cdguzman.tecnm.mx>
2024-11-17 02:08:16 +0000

    creación de la carpeta crud y la función find para listar productos

---
+++ crud/find.js
@@ -0,0 +1,11 @@
+require('../connection')
+
+const Product = require('../models/Product')
+
+async function main() {
+    const products = await Product.find()
+    console.log(products)    
+}
+
+
+main()
